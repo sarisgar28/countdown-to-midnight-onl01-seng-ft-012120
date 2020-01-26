3fd7bbfe027f9ead5fe819438243319287ebbe60
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-26 02:38:29 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
-
-def countdown
+# using_while
+x = 0 
+while x < 10 
+puts " # {x} is less than 10"
+x -= 10
+nil 
+while "# {10} SECOND(S)!"
+def countdown_with_sleep
+puts "HAPPY NEW YEAR!"
+nil 
 end
